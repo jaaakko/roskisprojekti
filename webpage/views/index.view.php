c6bee3f1821a14f72c998eb5ab29bbeede5f6ee4
jaaakko <jaakkohuotari04gmail.com>
2021-11-25 13:03:08 +0200

    trash_level 1.0

--- webpage/views/index.view.php
+++ webpage/views/index.view.php
@@ -4,9 +4,42 @@
 
 <?php
 foreach($allData as $sensordata)?>
-<div class="trash_unit">
+<div class="trash_data">
 <h2>Roskan tyyppi: <?=$sensordata["type"]?></h2>
-<p>Roskan etäisyys kannesta: <?=$sensordata["value"]?></p>
+<p>Roskan etäisyys kannesta: <?=$sensordata["value"], " " ,$sensordata["units"]?></p>
+<?php if($sensordata["value"] <= 22) {
+    echo "<div class='full'></div>";
+    echo "<div class='full'></div>";
+    echo "<div class='full'></div>";
+    echo "<div class='full'></div>";
+
+} else if($sensordata["value"] <= 40 && $sensordata["value"] > 22) {
+    echo "<div class='empty'></div>";
+    echo "<div class='almost_full'></div>";
+    echo "<div class='almost_full'></div>";
+    echo "<div class='almost_full'></div>";
+
+} else if($sensordata["value"] <=60 && $sensordata["value"] > 40) {
+    echo "<div class='empty'></div>";
+    echo "<div class='empty'></div>";
+    echo "<div class='semi_full'></div>";
+    echo "<div class='semi_full'></div>";
+
+} else if($sensordata["value"] <=80 && $sensordata["value"] > 60) {
+    echo "<div class='empty'></div>";
+    echo "<div class='empty'></div>";
+    echo "<div class='empty'></div>";
+    echo "<div class='almost_empty'></div>";
+
+} else if($sensordata["value"] > 80) {
+    echo "<div class='empty'></div>";
+    echo "<div class='empty'></div>";
+    echo "<div class='empty'></div>";
+    echo "<div class='empty'></div>";
+}
+
+
+?>
 </div>
 
 <?php require "partials/footer.php"; ?>
