c24b2a403fbd154aa8c5b64057d602ba0c5e666e
E-K-P <69452287+E-K-P@users.noreply.github.com>
2021-12-08 09:22:38 +0200

    Add files via upload

--- port_listener.py
+++ port_listener.py
@@ -1,18 +1,24 @@
 import mysql.connector
 import serial
+import datetime
 
 mydb = mysql.connector.connect(
     host="truudeli18.net",
     user="truu18_monkey",
     password="foRy7GfA6YTUgRtE",
     database="truu18_smart-trash-project"
+
+    #host="localhost",
+    #user="root",
+    #password="",
+    #database="insert_database_name"
     )
 
 print(mydb);
-
 print("listening to serial port COM5")
+print("reading datetime")
 serialPort = serial.Serial(port = "COM5", baudrate=9600,
-                           bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
+bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
                            
 serialString = ""                           # Used to hold data coming over UART
 
@@ -24,15 +30,25 @@ while(1):
         # Read data out of the buffer until a carraige return / new line is found
         serialString = serialPort.readline()
         value = serialString.decode('Ascii')
+        x = datetime.datetime.now()
+        y = x.strftime("%Y-%m-%d %H:%M:%S")
 
         mycursor = mydb.cursor()
+        sql = "SELECT * FROM sensordata;"
+        mycursor.execute(sql)
+        hist_data = mycursor.fetchall()
+        for row in hist_data:
+                print(row[0],row[1],row[2],row[3],row[4])
+                sql = "INSERT INTO sensordata_hist (sensorID, type, value, units, time) VALUES ('" + str(row[0]) + "', '" + str(row[1]) + "', '" + str(row[2]) + "', '" + str(row[3]) + "', '" + str(row[4]) + "');"
+                mycursor.execute(sql)
+                print("Moved previous records")
         
-        sql = "UPDATE sensordata SET value='" + value + "' WHERE sensorID = 1;";
-        
+        sql = "UPDATE sensordata SET value='" + value + "', time='" + y + "' WHERE sensorID = 1;";
         mycursor.execute(sql)
         mydb.commit()
 
         print(mycursor.rowcount, "record(s) affected")
         
-        # Print the contents of the serial data
+        #Print the contents of the serial data
         print(serialString.decode('Ascii'))
+        print(y)
