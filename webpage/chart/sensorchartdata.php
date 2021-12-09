35a3ff0c1cd3805807f87df2d0d5f3a0c4fb3ec4
jaaakko <jaakkohuotari04gmail.com>
2021-12-09 14:42:56 +0200

    base for line chart

---
+++ webpage/chart/sensorchartdata.php
@@ -0,0 +1,38 @@
+<?php
+//setting header to json
+header('Content-Type: application/json');
+
+//database
+define('DB_HOST', 'truudeli18.net');
+define('DB_USERNAME', 'truu18_monkey');
+define('DB_PASSWORD', 'foRy7GfA6YTUgRtE');
+define('DB_NAME', 'truu18_smart-trash-project');
+
+//get connection
+$mysqli = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+if(!$mysqli){
+  die("Connection failed: " . $mysqli->error);
+}
+
+//query to get data from the table
+$query = sprintf("SELECT sensorID FROM sensordata_hist");
+
+//execute query
+$result = $mysqli->query($query);
+//print_r($result);
+
+//loop through the returned data
+$data = array();
+foreach ($result as $row) {
+  $data[] = $row;
+}
+
+//free memory associated with result
+$result->close();
+
+//close connection
+$mysqli->close();
+
+//now print the data
+print json_encode($data);
