9f0ebadb5f2baf4ea6ada43f26a75088a15a75b1
Eero-Kasper Paloneva <Eero-kasper.paloneva@edu.tampere.fi>
2021-12-15 14:14:38 +0200

    "updates"

--- webpage/database/models/sensordata.php
+++ webpage/database/models/sensordata.php
@@ -2,7 +2,7 @@
 
 function getAllSensorData() {
     $pdo = connectDB();
-    $sql = "SELECT * FROM sensordata";
+    $sql = "SELECT * FROM sensordata INNER JOIN trash_type ON sensordata.typeID = trash_type.typeID";
     $stm = $pdo->prepare($sql);
     $stm->execute();
 
